5bdf0a13f4d3bfb28f941f2369a705b1332d803b
Himel Majumder <himelmajumder1@gmail.com>
2024-04-17 21:32:40 +0200

    Actualiza la configuración de trustHosts en app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustHosts(at: ['https://noodle-educative.up.railway.app']);
+        // $middleware->trustHosts(at: ['https://noodle-educative.up.railway.app']);
         // $middleware->trustProxies();
     })
     ->withExceptions(function (Exceptions $exceptions) {
